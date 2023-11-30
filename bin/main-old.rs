9c2bec37d162f208766bc3d7b852736e7c19b799
canewsin <canews.in@gmail.com>
2023-11-30 23:17:33 +0530

    basic example

---
+++ bin/main-old.rs
@@ -0,0 +1,45 @@
+use futures::prelude::*;
+use libp2p::swarm::{KeepAlive, NetworkBehaviour, Swarm, SwarmEvent};
+use libp2p::{identity, ping, Multiaddr, NetworkBehaviour, PeerId};
+use std::error::Error;
+
+#[tokio::main]
+async fn main() -> Result<(), Box<dyn Error>> {
+    let local_key = identity::Keypair::generate_ed25519();
+    let local_peer_id = PeerId::from(local_key.public());
+    println!("Local peer id: {local_peer_id:?}");
+
+    let transport = libp2p::tokio_development_transport(local_key).await?;
+
+    let mut swarm = Swarm(transport, Behaviour::default(), local_peer_id);
+
+    // Tell the swarm to listen on all interfaces and a random, OS-assigned
+    // port.
+    swarm.listen_on("/ip4/0.0.0.0/tcp/0".parse()?)?;
+
+    // Dial the peer identified by the multi-address given as the second
+    // command-line argument, if any.
+    if let Some(addr) = std::env::args().nth(1) {
+        let remote: Multiaddr = addr.parse()?;
+        swarm.dial(remote)?;
+        println!("Dialed {addr}")
+    }
+
+    loop {
+        match swarm.select_next_some().await {
+            SwarmEvent::NewListenAddr { address, .. } => println!("Listening on {address:?}"),
+            SwarmEvent::Behaviour(event) => println!("{event:?}"),
+            _ => {}
+        }
+    }
+}
+
+/// Our network behaviour.
+///
+/// For illustrative purposes, this includes the [`KeepAlive`](behaviour::KeepAlive) behaviour so a continuous sequence of
+/// pings can be observed.
+#[derive(NetworkBehaviour, Default)]
+struct Behaviour {
+    keep_alive: KeepAlive,
+    ping: ping::Behaviour,
+}
